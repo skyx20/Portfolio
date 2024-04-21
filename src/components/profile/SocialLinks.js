763c6f5c98d990505edb59c55afbd071205db2b1
skyx20 <delmasprower@gmail.com>
2024-04-21 18:41:22 -0400

    links fixed

--- src/components/profile/SocialLinks.js
+++ src/components/profile/SocialLinks.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CTA = () =>{
     return (
         <div className='flex items-center m-4'>
-            <Link className='m-2' to='linkedin.com/in/diego-cadenas/'><box-icon color='#000' size='sm' name='linkedin' type='logo'></box-icon></Link>
+            <Link className='m-2' to='https://linkedin.com/in/diego-cadenas/'><box-icon color='#000' size='sm' name='linkedin' type='logo'></box-icon></Link>
             <Link className='m-2' to='https://github.com/skyx20'><box-icon color='#000' size='sm' name='github' type='logo'></box-icon></Link>
 
         </div>
