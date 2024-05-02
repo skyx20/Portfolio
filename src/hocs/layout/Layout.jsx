7fa5d0adc9df2e7069b33443510406e475a95c43
skyx20 <delmasprower@gmail.com>
2024-05-02 17:26:28 -0400

    breakpoint positions of menu done

--- src/hocs/layout/Layout.jsx
+++ src/hocs/layout/Layout.jsx
@@ -7,9 +7,10 @@ const Layout = ({children}) => {
 
 
     return ( 
-        <div className="flex p-4 sm:p-8 transition-all
-        duration-400 ease-in-out md:p-20 h-screen">
-            <div className='hidden lg:block p-4 pr-6 relative left-4 w-64
+        <div className="flex p-4 sm:p-8 
+        transition-all duration-400 ease-in-out md:p-20 h-screen">
+            <div className='
+            hidden lg:block p-4 pr-6 relative left-4 w-64
             bg-cyan-300 overflow-auto border-cyan-400 rounded-l-2xl
             shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]'>
                 <ProfileCard/>
@@ -23,7 +24,9 @@ const Layout = ({children}) => {
             <div className='hidden lg:block flex-initial px-2'>
                 <Navbar/>
             </div>
-            <div className='lg:hidden px-2 sm:mx-2 sm:px-2'>
+            <div className='lg:hidden px-2 sm:mx-2 sm:px-2 sm:static 
+            absolute z-20
+            right-4 top-4'>
                 <RespNavbar/>
             </div>
         </div>
