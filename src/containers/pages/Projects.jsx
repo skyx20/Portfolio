02da48dd69af5929af77926c85eb068677181b07
skyx20 <dc.linkups@gmail.com>
2024-05-09 23:22:39 -0400

    updated

--- src/containers/pages/Projects.jsx
+++ src/containers/pages/Projects.jsx
@@ -1,32 +1,34 @@
 import ProjectCard from "../../components/projects/ProjectCard";
 import Layout from "../../hocs/layout/Layout";
-import p1  from "../../assets/images/projects/p1.png";
+import p1 from "../../assets/images/projects/p1.png";
 
-const data = 
-[
-    {
-        'id': '1',
-        'description': 'something',
-        'title': 'Marketing Agency',
-        'url': p1
-    },
+const data =
+    [
+        {
+            'id': '1',
+            'description': 'something',
+            'title': 'Marketing Agency',
+            'img': p1,
+            'url-github': 'https://marketing-agency-plum-rho.vercel.app/',
+            'url-deploy': 'https://github.com/skyx20/marketing-agency'
+        },
 
-]
+    ]
 
 
 const Projects = () => {
-    return ( 
+    return (
         <Layout>
             <div className='overflow-auto w-full h-full p-4'>
                 <div class="flex overflow-auto items-center justify-center">
                     <div class="grid mt-14 sm:mt-8 grid-cols-1 gap-5 xl:grid-cols-2 2xl:grid-cols-3">
-                        <ProjectCard data={data}/>
+                        <ProjectCard data={data} />
                     </div>
                 </div>
             </div>
-        
+
         </Layout>
     );
 }
- 
+
 export default Projects;
