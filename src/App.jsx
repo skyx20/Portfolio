bc2fe7727dc2c42e2d03f1265391b2ccb06ae7f4
skyx20 <delmasprower@gmail.com>
2024-12-17 17:41:36 -0300

    added vercel analitics

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./containers/pages/Home";
 import Contact from "./containers/pages/Contact";
 import Projects from "./containers/pages/Projects";
 import React  from 'react';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Analytics/>
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/projects/' element={<Projects/>} />
         <Route exact path='/contact/' element={<Contact/>} />
