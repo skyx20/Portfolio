3491760f97ae820a6c45d1ac2da12a6425ead622
skyx20 <dc.linkups@gmail.com>
2025-07-21 19:01:36 -0300

    fix(Route): Fix white page when routing to a unexistent route path

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./containers/pages/Home";
 import Contact from "./containers/pages/Contact";
 import Projects from "./containers/pages/Projects";
-import React  from 'react';
-import { Analytics } from "@vercel/analytics/react"
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Router>
-        <Analytics/>
+      <Analytics />
       <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route exact path='/projects/' element={<Projects/>} />
-        <Route exact path='/contact/' element={<Contact/>} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="*" element={<Projects />} />
+        <Route exact path="/projects/" element={<Projects />} />
+        <Route exact path="/contact/" element={<Contact />} />
       </Routes>
     </Router>
   );
